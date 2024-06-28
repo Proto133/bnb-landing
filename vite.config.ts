303c2dde3619f1e0ad2f48b7dbe69f7285e577aa
Peter Roto <peterdroto@gmail.com>
2024-06-28 17:42:18 -0500

    setting up gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/bnb-landing/',
   plugins: [
     vue({
       template: { transformAssetUrls }
